18d7fb74a0d1fa8fcf84c0f0c7da8ea5109b081f
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2023-06-19 12:07:25 +0530

    Fixes Metro init bug

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,10 +1,12 @@
+import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { greeting } from '.'
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
+            <Text>{greeting}</Text>
             <StatusBar style="auto" />
         </View>
     )
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#6933dccf',
         alignItems: 'center',
         justifyContent: 'center',
     },
--- packages/app/babel.config.js
+++ packages/app/babel.config.js
@@ -1,4 +1,6 @@
-module.exports = function (api) {
+module.exports = function (
+    /** @type {{ cache: (arg0: boolean) => void; }} */ api
+) {
     api.cache(true)
     return {
         presets: ['babel-preset-expo'],
